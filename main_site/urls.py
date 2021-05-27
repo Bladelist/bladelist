d0c9b364e80652af2281982145902230b6d69fda
ryuga <vaishnav2353@gmail.com>
2021-05-27 19:31:38 +0530

    Add bot page

--- main_site/urls.py
+++ main_site/urls.py
@@ -1,8 +1,9 @@
 
 from django.contrib import admin
 from django.urls import path
-from .views import index_view
+from .views import index_view, bot_view
 
 urlpatterns = [
     path('', index_view),
+    path('bots', bot_view)
 ]
--- main_site/views.py
+++ main_site/views.py
@@ -3,4 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 
 def index_view(request):
-    return render(request, "index.html")
+    return render(request, "index.html", {"search": True})
+
+def bot_view(request):
+    return render(request, "bots.html")
