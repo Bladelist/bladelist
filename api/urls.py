888c6ae969a399f46669c122fe2073f67f1eda8d
ryuga <vaishnav2353@gmail.com>
2021-06-09 22:07:16 +0530

    New endpoint for migrating user data

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import BotManageView
+from .views import BotManageView, UserMigrateView
 
 urlpatterns = [
     path('bot/<str:bot_id>/', BotManageView.as_view(), name='bot_manage'),
+    path('migrate/user/', UserMigrateView.as_view(), name='migrate_user'),
 ]
