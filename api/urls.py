6803a2608c8064cfe2c24782c2e35fd1700eb3bd
ryuga <vaishnav2353@gmail.com>
2021-08-12 11:27:53 +0530

    Routes for bot migration

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import BotManageView, UserMigrateView
+from .views import BotManageView, UserMigrateView, BotMigrateView
 
 urlpatterns = [
     path('bot/<str:bot_id>/', BotManageView.as_view(), name='bot_manage'),
     path('migrate/user/', UserMigrateView.as_view(), name='migrate_user'),
-    path('migrate/bot/', UserMigrateView.as_view(), name='migrate_user'),
+    path('migrate/bot/', BotMigrateView.as_view(), name='migrate_bot'),
 ]
