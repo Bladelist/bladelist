62fcf032c3cb703b9a0bbc1bb1606c7cf467dab2
ryuga <vaishnav2353@gmail.com>
2021-08-17 18:02:17 +0530

    Fix server API views

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import BotManageView, UserMigrateView, BotMigrateView, BotStatusEditView
+from .views import BotManageView, UserMigrateView, BotMigrateView, BotStatusEditView, ServerView
 
 urlpatterns = [
     path('bot/<str:bot_id>/', BotManageView.as_view(), name='bot_manage'),
+    path('server/<str:server_id>/', ServerView.as_view(), name='server_view'),
     path('migrate/user/', UserMigrateView.as_view(), name='migrate_user'),
     path('migrate/bot/', BotMigrateView.as_view(), name='migrate_bot'),
     path('bot/status/<int:bot_id>/', BotStatusEditView.as_view(), name='bot_status'),
--- api/views.py
+++ api/views.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 from rest_framework.views import APIView
-from main_site.models import Bot, BotTag, Member
+from main_site.models import Bot, BotTag, Member, Server
 from utils.mixins import ResponseMixin
 from rest_framework.response import Response
 from .serializers import BotSerializer
@@ -110,19 +110,7 @@ class ServerView(APIView, ResponseMixin):
 
     serializers = BotSerializer
 
-    def get(self, request, bot_id):
-        queryset = get_object_or_404(Bot, id=bot_id)
+    def get(self, request, server_id):
+        queryset = get_object_or_404(Server, id=server_id)
         serializer = self.serializers(queryset)
         return Response(serializer.data, status=200)
-
-    def put(self, request, bot_id):
-        queryset = get_object_or_404(Bot, id=bot_id)
-        if request.user.member == queryset.owner:
-            if request.data.get("server_count"):
-                queryset.server_count = request.data.get("server_count")
-                queryset.save()
-            if request.data.get("shard_count"):
-                queryset.meta.shard_count = request.data.get("shard_count")
-                queryset.meta.save()
-            return self.json_response_200()
-        return self.json_response_403()
