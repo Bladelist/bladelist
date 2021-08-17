4f2c67c31541039d1a28d74f3d0ed860e15ac5b0
ryuga <vaishnav2353@gmail.com>
2021-08-17 18:01:04 +0530

    Add server api view and serializers

--- api/serializers.py
+++ api/serializers.py
@@ -1,11 +1,11 @@
 from rest_framework import serializers
-from main_site.models import Bot, BotMeta
+from main_site.models import Bot, BotMeta, Server, ServerMeta
 
 
 class BotMetaSerializer(serializers.ModelSerializer):
     class Meta:
         model = BotMeta
-        exclude = ("id", "bot", "long_desc", "moderator")
+        exclude = ("id", "bot", "long_desc", "moderator", "rejection_reason", "rejection_count")
 
 
 class BotSerializer(serializers.ModelSerializer):
@@ -27,3 +27,17 @@ class BotStatusSerializer(serializers.ModelSerializer):
     class Meta:
         model = Bot
         fields = ["verified", "banned", "verification_status"]
+
+
+class ServerMetaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ServerMeta
+        exclude = ("id", "server", "long_desc", "moderator", "rejection_reason", "rejection_count")
+
+
+class ServerSerializer(serializers.ModelSerializer):
+    meta = ServerMetaSerializer()
+
+    class Meta:
+        model = Server
+        fields = ("id", "name", "invite_link", "votes", "short_desc", "members_online", "is_nsfw", "meta")
--- api/views.py
+++ api/views.py
@@ -104,3 +104,25 @@ class BotStatusEditView(APIView, ResponseMixin):
                 return Response(serializer.data)
             return self.json_response_400()
         return self.json_response_401()
+
+
+class ServerView(APIView, ResponseMixin):
+
+    serializers = BotSerializer
+
+    def get(self, request, bot_id):
+        queryset = get_object_or_404(Bot, id=bot_id)
+        serializer = self.serializers(queryset)
+        return Response(serializer.data, status=200)
+
+    def put(self, request, bot_id):
+        queryset = get_object_or_404(Bot, id=bot_id)
+        if request.user.member == queryset.owner:
+            if request.data.get("server_count"):
+                queryset.server_count = request.data.get("server_count")
+                queryset.save()
+            if request.data.get("shard_count"):
+                queryset.meta.shard_count = request.data.get("shard_count")
+                queryset.meta.save()
+            return self.json_response_200()
+        return self.json_response_403()
