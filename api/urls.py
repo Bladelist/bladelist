b48f29b37f64aad408283d4dbb888792bb485844
ryuga <vaishnav2353@gmail.com>
2021-10-08 21:17:25 +0530

    add support

--- api/urls.py
+++ api/urls.py
@@ -3,10 +3,12 @@ from .views import BotManageView, ServerManageView
 from .private_views import BotStatusEditView, ServerStatusEditView  # UserMigrateView, BotMigrateView,
 
 urlpatterns = [
-    path('bot/<str:bot_id>/', BotManageView.as_view(), name='bot_manage'),
+    path('bots/<str:bot_id>', BotManageView.as_view(), name='bot_manage'),
+    path('bots/<str:bot_id>/', BotManageView.as_view(), name='bot_manage_alt'),
     path('bot/status/<int:bot_id>/', BotStatusEditView.as_view(), name='bot_status'),
     # path('bots/', BotManageView.as_view(), name='bot_manage_dev'),
-    path('server/<str:server_id>/', ServerManageView.as_view(), name='server_view'),
+    path('server/<str:server_id>', ServerManageView.as_view(), name='server_view'),
+    path('server/<str:server_id>/', ServerManageView.as_view(), name='server_view_alt'),
     path('server/status/<int:server_id>/', ServerStatusEditView.as_view(), name='server_status'),
     # path('migrate/user/', UserMigrateView.as_view(), name='migrate_user'),
     # path('migrate/bot/', BotMigrateView.as_view(), name='migrate_bot'),
